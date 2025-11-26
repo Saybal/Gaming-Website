9ba209f896828105bce32e3a10b6bf9f04bfb86d
Saybal <roysaybal@gmail.com>
2025-11-26 17:23:19 +0600

    Mdifying Featrue Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
-import './App.css'
-import About from './Component/About'
-import Features_Section from './Component/Featured_Section/Features_Section'
-import Hero from './Component/Hero'
-import NavBar from './Component/NavBar'
+import "./App.css";
+import About from "./Component/About";
+import Features_Section from "./Component/Featured_Section/Features_Section";
+import Hero from "./Component/Hero";
+import NavBar from "./Component/NavBar";
+
+// #e7e5e4
 
 function App() {
-  
-
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <NavBar/>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+        {/* Dashed Grid */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+        linear-gradient(to right, #91A3B0 1px, transparent 1px),
+        linear-gradient(to bottom, #91A3B0 1px, transparent 1px)
+      `,
+          backgroundSize: "20px 20px",
+          backgroundPosition: "0 0, 0 0",
+          maskImage: `
+        repeating-linear-gradient(
+          to right,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        ),
+        repeating-linear-gradient(
+          to bottom,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        )
+      `,
+          WebkitMaskImage: `
+        repeating-linear-gradient(
+          to right,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        ),
+        repeating-linear-gradient(
+          to bottom,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        )
+      `,
+          maskComposite: "intersect",
+          WebkitMaskComposite: "source-in",
+        }}
+      />
+      <NavBar />
       <Hero />
       <About />
-      <Features_Section/>
+      <Features_Section />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Featured_Section/Features_Section.jsx
+++ src/Component/Featured_Section/Features_Section.jsx
@@ -1,89 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Featured_Card from "./Featured_Card";
 import { TiLocationArrow } from "react-icons/ti";
+import axios from "axios";
 
-export const featured_card_Data = [
-  {
-    id: 1,
-    src: "videos/feature-1.mp4",
-    title: (
-      <>
-        radia<b>n</b>t
-      </>
-    ),
-    description:
-      "A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure.",
-    isComingSoon: true,
-    wrapperClass:
-      "border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]",
-    type: "card",
-  },
-  {
-    id: 2,
-    src: "videos/feature-2.mp4",
-    title: (
-      <>
-        zig<b>m</b>a
-      </>
-    ),
-    description:
-      "An anime and gaming-inspired NFT collection - the IP primed for expansion.",
-    isComingSoon: true,
-    wrapperClass: "bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2",
-    type: "card",
-  },
-  {
-    id: 3,
-    src: "videos/feature-3.mp4",
-    title: (
-      <>
-        n<b>e</b>xus
-      </>
-    ),
-    description:
-      "A gamified social hub, adding a new dimension of play to social interaction for Web3 communities.",
-    isComingSoon: true,
-    wrapperClass: "bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0",
-    type: "card",
-  },
-  {
-    id: 4,
-    src: "videos/feature-4.mp4",
-    title: (
-      <>
-        az<b>u</b>l
-      </>
-    ),
-    description:
-      "A cross-world AI Agent - elevating your gameplay to be more fun and productive.",
-    isComingSoon: true,
-    wrapperClass: "bento-tilt_1 me-14 md:col-span-1 md:me-0",
-    type: "card",
-  },
-  {
-    id: 5,
-    type: "textBox",
-    wrapperClass: "bento-tilt_2",
-    content: (
+const Features_Section = () => {
+  const [featured_card_Data, setfeatured_card_Data] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("/Json/Featured_Card_Data.json")
+      .then((response) => {
+        // console.log(response);
+        setfeatured_card_Data(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching featured card data:", error);
+      });
+  }, []);
+
+  // console.log(featured_card_Data);
+
+  const formatTitle = (t) => {
+  return (
+    <span
+      dangerouslySetInnerHTML={{ __html: t }}
+    />
+  );
+};
+
+
+  const TextBox = ({content}) => {
+    return (
+      
       <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
         <h1 className="bento-title special-font max-w-64 text-black">
-          M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
+          {formatTitle(content)}
         </h1>
         <TiLocationArrow className="m-5 scale-[5] self-end" />
       </div>
-    ),
-  },
-  {
-    id: 6,
-    type: "videoBox",
-    wrapperClass: "bento-tilt_2",
-    videoSrc: "videos/feature-5.mp4",
-  },
-];
+  
+    );
+  }
+  if (isLoading) return null;
 
-const Features_Section = () => {
   return (
-    <section className="bg-black pb-52">
+    <section className="bg-black relative z-1 pb-52">
       <div className="max-auto container px-3 md:px-10">
         <div className="px-5 py-32">
           <p className="font-circular text-lg text-blue-2-50">
@@ -99,7 +62,7 @@ const Features_Section = () => {
         <div className={featured_card_Data[0].wrapperClass}>
           <Featured_Card
             src={featured_card_Data[0].src}
-            title={featured_card_Data[0].title}
+            title={(formatTitle(featured_card_Data[0].title))}
             description={featured_card_Data[0].description}
             isComingSoon={featured_card_Data[0].isComingSoon}
           />
@@ -112,13 +75,13 @@ const Features_Section = () => {
               {item.type === "card" && (
                 <Featured_Card
                   src={item.src}
-                  title={item.title}
+                  title={formatTitle(item.title)}
                   description={item.description}
                   isComingSoon={item.isComingSoon}
                 />
               )}
 
-              {item.type === "textBox" && item.content}
+              {item.type === "textBox" && <TextBox content={item.content} />}
 
               {item.type === "videoBox" && (
                 <video
